c21701231197e30d59cbf843a72f2dc621e1316a
shocker945516 <shocker945516@gmail.com>
2022-07-06 16:13:10 -0700

    Became to a dice game

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import java.util.InputMismatchException;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,45 +9,75 @@ public class App {
         Scanner input = new Scanner(System.in);
         System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         System.out.println("x                   Welcome                   x");
-        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-        while (poker.getBalance() > 0) {
-            // try {
-            System.out.println(poker.toString());
-            Thread.sleep(1200);
-            System.out.println("Place your bet: ");
-            Integer bet = input.nextInt();
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
 
-            if (bet > poker.getBalance()) {
-                System.out.println("Insufficient fund!!");
+        while (poker.getBalance() > 0) {
+            try {
+                System.out.println(poker.toString());
                 Thread.sleep(1200);
-                System.out.println("===========================");
-                continue;
+                System.out.println("Choose the side you want to bet");
+                System.out.println("(1): Red");
+                System.out.println("(2): Blue");
+                System.out.println("(3): Tie");
+                Integer side = input.nextInt();
+                if (side > 3) {
+                    System.out.println("Please enter 1, 2 or 3");
+                    Thread.sleep(1200);
+                    clrscr();
+                    continue;
+                }
+
+                Thread.sleep(1200);
+                System.out.println("Place your bet: ");
+                Integer bet = input.nextInt();
+
+                if (bet > poker.getBalance()) {
+                    System.out.println("Insufficient fund!!");
+                    Thread.sleep(1200);
+                    continue;
+                }
+                int red = rand.nextInt(6) + 1;
+                int blue = rand.nextInt(6) + 1;
+                Thread.sleep(1200);
+                System.out.println("The point of red is: " + red);
+                Thread.sleep(1200);
+                System.out.println("The point of blue is: " + blue);
+                Thread.sleep(1200);
+                switch (side) {
+                    case 1:
+                        poker.SideOfRed(bet, red, blue);
+                        break;
+                    case 2:
+                        poker.SideOfBlue(bet, red, blue);
+                        break;
+                    case 3:
+                        poker.SideOfTie(bet, red, blue);
+                        break;
+                }
+                Thread.sleep(2000);
+                clrscr();
+            } catch (Exception e) {
+                System.out.println("Please enter vaild values");
+                input.next();
+                Thread.sleep(1200);
+                clrscr();
             }
-            int player = rand.nextInt(13) + 1;
-            int banker = rand.nextInt(13) + 1;
-            Thread.sleep(1200);
-            System.out.println("Your card is: " + player);
-            Thread.sleep(1200);
-            System.out.println("Banker's card is: " + banker);
-            Thread.sleep(1200);
-            if (player > banker) {
-                poker.Win(bet);
-                System.out.println("You Win!");
-            } else if (player == banker) {
-                System.out.println("Tie!");
-            } else if (player < banker) {
-                poker.Lose(bet);
-                System.out.println("You Lose!");
-            }
-            System.out.println("===========================");
-            Thread.sleep(1200);
-            // } catch (InputMismatchException e) {
-            // System.out.println("Please enter vaild numbers");
-            // }
         }
         input.close();
         System.out.println("You've lost all your money.");
         System.out.println("---------GAME OVER---------");
 
+    }
+
+    public static void clrscr() {
+        // Clears Screen in java
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+            System.out.print("\033\143");
+        } catch (IOException | InterruptedException ex) {
+        }
     }
 }
--- src/Poker.java
+++ src/Poker.java
@@ -1,5 +1,5 @@
 public class Poker {
-    private int balance = 100;
+    private int balance = 20;
 
     @Override
     public String toString() {
@@ -18,4 +18,52 @@ public class Poker {
         balance = balance - bet;
     }
 
+    void LoseDouble(int bet) {
+        balance = balance - bet * 2;
+    }
+
+    void SideOfRed(int bet, int red, int blue) {
+        if (red > blue) {
+            Win(bet);
+            System.out.println("Red Wins. You Won!");
+            System.out.println("Congratulations!!");
+        } else if (red < blue) {
+            Lose(bet);
+            System.out.println("Blue Wins. You Lost");
+        } else {
+            LoseDouble(bet);
+            System.out.println("Tie!");
+            System.out.println("You Lost Double of Your Bet!");
+        }
+    }
+
+    void SideOfBlue(int bet, int red, int blue) {
+        if (red < blue) {
+            Win(bet);
+            System.out.println("Blue Wins. You Won!");
+            System.out.println("Congratulations!!");
+        } else if (red > blue) {
+            Lose(bet);
+            System.out.println("Red Wins. You Lost");
+        } else {
+            LoseDouble(bet);
+            System.out.println("Tie!");
+            System.out.println("You Lost Double of Your Bet!");
+        }
+    }
+
+    void SideOfTie(int bet, int red, int blue) {
+        if (red == blue) {
+            balance = balance + bet * 4;
+            System.out.println("Tie. You Won!");
+            System.out.println("Congratulations!! You Got 4 Times of Your Bet!!!!");
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+
+        } else {
+            Lose(bet);
+            System.out.println("You Lost!");
+        }
+    }
 }
